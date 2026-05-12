34f5547aff5643fa3646b17e74c8a85c74c9f667
DR CHAMPAK MALIVAD <149330753+CSMALIVAD@users.noreply.github.com>
2026-05-12 15:54:06 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
+import "./interactions.js";
 
+import {
+createLinear
+}
+from "./creators.js";
+
+import {
+saveData,
+loadData
+}
+from "./storage.js";
+
+import {
+adminLogin,
+applyChanges
+}
+from "./admin.js";
+
+
+// BUTTONS
+
+document
+.getElementById("linearBtn")
+.addEventListener(
+"click",
+createLinear
+);
+
+document
+.getElementById("saveBtn")
+.addEventListener(
+"click",
+saveData
+);
+
+document
+.getElementById("loadBtn")
+.addEventListener(
+"click",
+loadData
+);
+
+document
+.getElementById("adminBtn")
+.addEventListener(
+"click",
+adminLogin
+);
+
+document
+.getElementById("applyBtn")
+.addEventListener(
+"click",
+applyChanges
+);
