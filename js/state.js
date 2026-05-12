ff9cb1eeef67fd25e78e1667a311d88dfc73cccd
DR CHAMPAK MALIVAD <149330753+CSMALIVAD@users.noreply.github.com>
2026-05-12 16:45:53 +0530

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,23 @@
+export const state={
 
+pathways:[],
+
+selectedObject:null,
+
+adminMode:false,
+
+scale:1,
+
+panX:0,
+
+panY:0,
+
+isDragging:false,
+
+dragNode:null,
+
+startX:0,
+
+startY:0
+
+};
