5b5e97ad71d681c56d51ee8f8ed0260892abb48d
DR CHAMPAK MALIVAD <149330753+CSMALIVAD@users.noreply.github.com>
2026-05-12 16:50:33 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,43 @@
+import { state }
+from "./state.js";
 
+import {
+render
+}
+from "./renderer.js";
+
+export function saveData(){
+
+localStorage.setItem(
+
+"biochampData",
+
+JSON.stringify(
+state.pathways
+)
+
+);
+
+alert("Saved");
+
+}
+
+export function loadData(){
+
+const data=
+localStorage.getItem(
+"biochampData"
+);
+
+if(data){
+
+state.pathways=
+JSON.parse(data);
+
+render();
+
+alert("Loaded");
+
+}
+
+}
