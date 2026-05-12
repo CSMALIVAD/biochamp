bfdd0c9f399d91aeb979f9e6a7bbad0b1d4c7731
DR CHAMPAK MALIVAD <149330753+CSMALIVAD@users.noreply.github.com>
2026-05-12 16:08:40 +0530

    Update interactions.js

--- js/interactions.js
+++ js/interactions.js
@@ -1,1 +1,87 @@
+import { state }
+from "./state.js";
 
+import {
+updateTransform
+}
+from "./renderer.js";
+
+
+const svg =
+document.getElementById("viewer");
+
+
+// ZOOM
+
+svg.addEventListener(
+"wheel",
+(e)=>{
+
+e.preventDefault();
+
+if(e.deltaY<0){
+
+state.scale*=1.1;
+
+}
+else{
+
+state.scale*=0.9;
+
+}
+
+updateTransform();
+
+}
+);
+
+
+// PAN START
+
+svg.addEventListener(
+"mousedown",
+(e)=>{
+
+state.isDraggingCanvas=true;
+
+state.startX=
+e.clientX-state.panX;
+
+state.startY=
+e.clientY-state.panY;
+
+}
+);
+
+
+// PAN END
+
+window.addEventListener(
+"mouseup",
+()=>{
+
+state.isDraggingCanvas=false;
+
+}
+);
+
+
+// PAN MOVE
+
+window.addEventListener(
+"mousemove",
+(e)=>{
+
+if(!state.isDraggingCanvas)
+return;
+
+state.panX=
+e.clientX-state.startX;
+
+state.panY=
+e.clientY-state.startY;
+
+updateTransform();
+
+}
+);
